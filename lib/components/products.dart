bacab599388752535aaeef96de0dc36953a3578b
Erick Milan Marin <erickmilan1002@gmail.com>
2022-08-31 18:05:04 -0500

    subgrups

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -17,34 +17,39 @@ class Products extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
 
     List products = [
-      { 'name': 'Arroz Chaufa',   'price': 21, 'id': 1,  'idProduct': 1,  'category': 1,  'imgPath': "images/bacon.png"},
-      { 'name': "Ensalada pollo", 'price': 12, 'id': 27, 'idProduct':27,  'category': 27, 'imgPath': 'images/polloSano.png'},
-      { 'name': "Pasta",          'price': 18, 'id': 15, 'idProduct': 15, 'category': 15, 'imgPath': "images/tallarin.png"},
-      { 'name': "Ensalada Atun",  'price': 15, 'id': 25, 'idProduct': 25, 'category': 25, 'imgPath': "images/atun.png"},
-      { 'name': "Alitas pollo",   'price': 15, 'id': 26, 'idProduct': 26, 'category': 26, 'imgPath': "images/alitas.png"},
-      { 'name': "Filete de Res",  'price': 18, 'id': 2,  'idProduct': 2,  'category': 2,  'imgPath': "images/fileteRes.png"},
-      { 'name': "Chorizo",        'price': 21, 'id': 13, 'idProduct': 13, 'category': 13, 'imgPath': "images/chorizo.png"},
-      { 'name': "Tocino",         'price': 14, 'id': 3,  'idProduct': 3,  'category': 3,  'imgPath': "images/tocino.png"},
-      { 'name': "Pizza",          'price': 22, 'id': 6,  'idProduct': 6,  'category': 6,  'imgPath': "images/pizza.png"},
-      { 'name': "Taco",           'price': 12, 'id': 7,  'idProduct': 7,  'category': 7,  'imgPath': "images/taco.png"},
-      { 'name': "Hamburguer",     'price': 14, 'id': 8,  'idProduct': 8,  'category': 8,  'imgPath': "images/hamburguer.png"},
-      { 'name': "Sopa Toscana",   'price': 15, 'id': 28, 'idProduct': 28, 'category': 28, 'imgPath': "images/sopaToscana.png"},
-      { 'name': "Curry",          'price': 15, 'id': 20, 'idProduct': 20, 'category': 20, 'imgPath': "images/curry.png"},
-      { 'name': "Sopa Ramen",     'price': 12, 'id': 4,  'idProduct': 4,  'category': 4,  'imgPath': "images/soup.png"},
-      { 'name': "Sopa Todu",      'price': 11, 'id': 5,  'idProduct': 5,  'category': 5,  'imgPath': "images/Todu.png"},
-      { 'name': "Sashimi",        'price': 13, 'id': 11, 'idProduct': 11, 'category': 11, 'imgPath': "images/sashimi.png"},
-      { 'name': "Sopa India",     'price': 21, 'id': 29, 'idProduct': 29, 'category': 29, 'imgPath': "images/sopaIndia.png"},
-      { 'name': "Sidra",          'price': 14, 'id': 16, 'idProduct': 16, 'category': 16, 'imgPath': "images/Sidra.png"},
-      { 'name': "Pepsi",          'price': 17, 'id': 17, 'idProduct': 17, 'category': 17, 'imgPath': "images/pepsi.png"},
-      { 'name': "Martini",        'price': 21, 'id': 18, 'idProduct': 18, 'category': 18, 'imgPath': "images/martini.png"},
-      { 'name': "Heineken",       'price': 17, 'id': 22, 'idProduct': 22, 'category': 22, 'imgPath': "images/heineken.png"},
-      { 'name': "Fresa",          'price': 19, 'id': 19, 'idProduct': 19, 'category': 19, 'imgPath': "images/fresa.png"},
-      { 'name': "Vodka",          'price': 10, 'id': 10, 'idProduct': 10, 'category': 10, 'imgPath': "images/vodkaRojo.png"},
-      { 'name': "Cafe",           'price': 10, 'id': 24, 'idProduct': 24, 'category': 24, 'imgPath': "images/cafe.png"},
-      { 'name': "Vino de Mesa",   'price': 11, 'id': 9,  'idProduct': 9, 'category': 9,   'imgPath': "images/vinoMesa.png"},
-      { 'name': "Helado",         'price': 13, 'id': 12, 'idProduct': 12, 'category': 12, 'imgPath': "images/Helado.png"},
-      { 'name': "Chocolate",      'price': 18, 'id': 14, 'idProduct': 14, 'category': 14, 'imgPath': "images/chocolate.png"},
-      { 'name': "Te de Manzana",  'price': 17, 'id': 21, 'idProduct': 21, 'category': 21, 'imgPath': "images/manzana.png"},
+      { 'name': 'Arroz Chaufa',   'price': 21, 'id': 1,  'idProduct': 1,  'category':"comida", 'imgPath': "images/bacon.png"},
+      { 'name': "Ensalada pollo", 'price': 12, 'id': 27, 'idProduct':27,  'category':"comida", 'imgPath': 'images/polloSano.png'},
+      { 'name': "Pasta",          'price': 18, 'id': 15, 'idProduct': 15, 'category':"comida", 'imgPath': "images/tallarin.png"},
+      { 'name': "Ensalada Atun",  'price': 15, 'id': 25, 'idProduct': 25, 'category':"comida", 'imgPath': "images/atun.png"},
+
+      { 'name': "Alitas pollo",   'price': 15, 'id': 26, 'idProduct': 26, 'category':"carne", 'imgPath': "images/alitas.png"},
+      { 'name': "Filete de Res",  'price': 18, 'id': 2,  'idProduct': 2,  'category':"carne", 'imgPath': "images/fileteRes.png"},
+      { 'name': "Chorizo",        'price': 21, 'id': 13, 'idProduct': 13, 'category':"carne", 'imgPath': "images/chorizo.png"},
+      { 'name': "Tocino",         'price': 14, 'id': 3,  'idProduct': 3,  'category':"carne", 'imgPath': "images/tocino.png"},
+
+      { 'name': "Pizza",          'price': 22, 'id': 6,  'idProduct': 6,  'category':"pizza", 'imgPath': "images/pizza.png"},
+      { 'name': "Taco",           'price': 12, 'id': 7,  'idProduct': 7,  'category':"pizza", 'imgPath': "images/taco.png"},
+      { 'name': "Hamburguer",     'price': 14, 'id': 8,  'idProduct': 8,  'category':"pizza", 'imgPath': "images/hamburguer.png"},
+
+      { 'name': "Sopa Toscana",   'price': 15, 'id': 28, 'idProduct': 28, 'category':"sopa", 'imgPath': "images/sopaToscana.png"},
+      { 'name': "Curry",          'price': 15, 'id': 20, 'idProduct': 20, 'category':"sopa", 'imgPath': "images/curry.png"},
+      { 'name': "Sopa Ramen",     'price': 12, 'id': 4,  'idProduct': 4,  'category':"sopa", 'imgPath': "images/soup.png"},
+      { 'name': "Sopa Todu",      'price': 11, 'id': 5,  'idProduct': 5,  'category':"sopa", 'imgPath': "images/Todu.png"},
+      { 'name': "Sashimi",        'price': 13, 'id': 11, 'idProduct': 11, 'category':"sopa", 'imgPath': "images/sashimi.png"},
+      
+      { 'name': "Sidra",          'price': 14, 'id': 16, 'idProduct': 16, 'category':"bebida", 'imgPath': "images/Sidra.png"},
+      { 'name': "Pepsi",          'price': 17, 'id': 17, 'idProduct': 17, 'category':"bebida", 'imgPath': "images/pepsi.png"},
+      { 'name': "Te de Manzana",  'price': 17, 'id': 21, 'idProduct': 21, 'category':"bebida", 'imgPath': "images/manzana.png"},
+      { 'name': "Fresa",          'price': 19, 'id': 19, 'idProduct': 19, 'category':"bebida", 'imgPath': "images/fresa.png"},
+      { 'name': "Cafe",           'price': 10, 'id': 24, 'idProduct': 24, 'category':"bebida", 'imgPath': "images/cafe.png"},
+
+      { 'name': "Vino de Mesa",   'price': 11, 'id': 9,  'idProduct': 9,  'category':"alcohol", 'imgPath': "images/vinoMesa.png"},
+      { 'name': "Vodka",          'price': 10, 'id': 10, 'idProduct': 10, 'category':"alcohol", 'imgPath': "images/vodkaRojo.png"},
+      { 'name': "Heineken",       'price': 17, 'id': 22, 'idProduct': 22, 'category':"alcohol", 'imgPath': "images/heineken.png"},
+      { 'name': "Martini",        'price': 21, 'id': 18, 'idProduct': 18, 'category':"bebida", 'imgPath': "images/martini.png"},
+
+      { 'name': "Chocolate",      'price': 18, 'id': 14, 'idProduct': 14, 'category':" ", 'imgPath': "images/chocolate.png"},
+      { 'name': "Helado",         'price': 13, 'id': 12, 'idProduct': 12, 'category':" ", 'imgPath': "images/Helado.png"},
     ];
     return Padding(
       padding: EdgeInsets.only(
